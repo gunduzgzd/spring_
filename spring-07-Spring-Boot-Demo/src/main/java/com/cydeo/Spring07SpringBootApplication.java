61216363623096bb3fa0acc20caa84cb8c42e73d
gozdegunduz <gunduzgzd@gmail.com>
2024-03-24 00:25:51 -0400

    Spring-Boot

---
+++ spring-07-Spring-Boot-Demo/src/main/java/com/cydeo/Spring07SpringBootApplication.java
@@ -0,0 +1,30 @@
+package com.cydeo;
+
+import com.cydeo.model.Comment;
+import com.cydeo.service.CommentService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class Spring07SpringBootApplication {
+
+	public static void main(String[] args) {
+
+        Comment comment  = new Comment();
+        comment.setAuthor("Johnson");
+        comment.setText("Spring Framework");
+
+
+		ApplicationContext container = SpringApplication.run(Spring07SpringBootApplication.class, args);
+
+        CommentService commentService = container.getBean(CommentService.class);
+        commentService.publishComment(comment);
+
+        commentService.printConfigData();
+        commentService.printDBConfigData();
+
+
+	}
+
+}
