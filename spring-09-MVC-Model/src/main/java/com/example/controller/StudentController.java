061b976965e64693ae34d4a0fb960c3e33ac685b
gozdegunduz <gunduzgzd@gmail.com>
2024-03-27 16:08:24 -0400

    MVC Model created

--- spring-09-MVC-Model/src/main/java/com/example/controller/StudentController.java
+++ spring-09-MVC-Model/src/main/java/com/example/controller/StudentController.java
@@ -1,13 +1,20 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class StudentController {
 
     @RequestMapping("/welcome")
-    public String homePage(){
+    public String homePage(Model model){
+
+        //method Model Interface
+        model.addAttribute("name","Ada");
+        model.addAttribute("course","MVC");
+
+        return "student/welcome";
 
     }
 }
