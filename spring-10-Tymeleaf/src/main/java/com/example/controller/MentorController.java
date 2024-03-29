d9ad63c6c8b079071be61759b78e841232ec48d8
gozdegunduz <gunduzgzd@gmail.com>
2024-03-28 22:48:33 -0400

    Thymeleaf continue

---
+++ spring-10-Tymeleaf/src/main/java/com/example/controller/MentorController.java
@@ -0,0 +1,27 @@
+package com.example.controller;
+
+import com.example.bootstrap.DataGenerator;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mentor")
+public class MentorController {
+
+    @RequestMapping("/register") //localhost:8080/register
+    public String register(Model model){
+
+        model.addAttribute("students", DataGenerator.createStudent());
+
+        return "student/register";
+
+    }
+
+    @RequestMapping("/drop") //localhost:8080/register
+    public String drop(){
+
+        return "student/register";
+
+    }
+}
--- spring-10-Tymeleaf/src/main/java/com/example/model/Student.java
+++ spring-10-Tymeleaf/src/main/java/com/example/model/Student.java
@@ -7,6 +7,8 @@ import lombok.Data;
 @AllArgsConstructor
 public class Student {
 
+
+
     private String firstName;
     private String lastName;
     private int age;
