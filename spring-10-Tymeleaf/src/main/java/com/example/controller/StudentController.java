d1ca7940e31102185c65aee063a6141831e0ab35
gozdegunduz <gunduzgzd@gmail.com>
2024-04-04 13:40:45 -0400

    new annotations added

--- spring-10-Tymeleaf/src/main/java/com/example/controller/StudentController.java
+++ spring-10-Tymeleaf/src/main/java/com/example/controller/StudentController.java
@@ -4,14 +4,16 @@ package com.example.controller;
 import com.example.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("student")
 public class StudentController {
 
-    @RequestMapping("/register") //localhost:8080/register
+    @RequestMapping(value = "/register",method = RequestMethod.GET) //localhost:8080/register
     public String register(Model model){
 
         model.addAttribute("students", DataGenerator.createStudent());
@@ -20,14 +22,17 @@ public class StudentController {
 
     }
 
-
-
-    @RequestMapping("/welcome") //localhost:8080/welcome
-    public String drop(){
-
-
+    @RequestMapping("/welcome") //localhost:8080/student/welcome?name=Gozde
+    public String welcome1(@RequestParam String name){
 
         return "student/welcome";
+
+    }
+
+
+    @RequestMapping("/welcome/{name}") //localhost:8080/student/welcome/Gozde
+    public String welcome2(@PathVariable String name){
+   return "student/welcome";
 
     }
 
