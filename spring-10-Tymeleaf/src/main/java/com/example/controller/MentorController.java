e2fbef0aa78d0d01aa63736c186eddb15934a549
gozdegunduz <gunduzgzd@gmail.com>
2024-04-04 15:57:51 -0400

    new annotations added

--- spring-10-Tymeleaf/src/main/java/com/example/controller/MentorController.java
+++ spring-10-Tymeleaf/src/main/java/com/example/controller/MentorController.java
@@ -1,27 +1,50 @@
 package com.example.controller;
 
-import com.example.bootstrap.DataGenerator;
+import com.example.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/mentor")
 public class MentorController {
 
-    @RequestMapping("/register") //localhost:8080/register
-    public String register(Model model){
+    @GetMapping("/register") //localhost:8080/register
+    public String register(Model model) {
 
-        model.addAttribute("students", DataGenerator.createStudent());
+        List<String> batchList = Arrays.asList("JD1", "JD2", "JD3");
 
-        return "student/register";
+        model.addAttribute("mentor", new Mentor());
+        model.addAttribute("batchList", batchList);
+
+        return "mentor/register";
+
+    }
+
+    @PostMapping("/confirm")
+    public String submitForm(@ModelAttribute("mentor") Mentor mentor) {
+        //accessing the mentor attribute in this method
+
+
+        return "mentor/mentor-confirmation";
+
 
     }
 
-    @RequestMapping("/drop") //localhost:8080/register
-    public String drop(){
+    @GetMapping("/confirm")
+    public String submitForm2() {
 
-        return "student/register";
+        return "mentor/mentor-confirmation";
+
 
     }
+
+
 }
