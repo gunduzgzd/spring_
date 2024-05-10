2a71cb89eddba8ea8b0f5b033be2f2ca93aa9b8e
gozdegunduz <gunduzgzd@gmail.com>
2024-05-10 15:00:06 -0400

    Derived query has been completed JPQL started

---
+++ spring-13-DataQueries/src/main/java/com/example/spring13dataqueries/Spring13DataQueriesApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring13dataqueries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring13DataQueriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring13DataQueriesApplication.class, args);
+    }
+
+}
