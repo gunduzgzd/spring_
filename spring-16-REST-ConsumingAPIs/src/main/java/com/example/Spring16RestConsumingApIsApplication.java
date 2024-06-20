68c4a1404eda411e65453830738b1093c34577a7
gozdegunduz <gunduzgzd@gmail.com>
2024-06-20 17:43:17 -0400

    Json

--- spring-16-REST-ConsumingAPIs/src/main/java/com/example/Spring16RestConsumingApIsApplication.java
+++ spring-16-REST-ConsumingAPIs/src/main/java/com/example/Spring16RestConsumingApIsApplication.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +16,11 @@ public class Spring16RestConsumingApIsApplication {
 	@Bean
 	public RestTemplate restTemplate(){
 		return new RestTemplate();
+	}
+
+	@Bean
+	public ModelMapper mapper() {
+		return new ModelMapper();
 	}
 }
 
